e8e8e31c740c71773000bcdf5a3d4efe8bd2db31
efy26 <eliezerefoyaefy2018@gmail.com>
2025-06-25 09:45:12 -0400

    modification texte accueil

--- components/home/SectionUn.jsx
+++ components/home/SectionUn.jsx
@@ -8,15 +8,18 @@ export default function SectionUn() {
       id="section-un"
       className="bg-[url(@/public/img/bgJet.png)] bg-no-repeat flex  flex-col items-center"
     >
-      {/* <Link id="link-home" href="/" className="size-40 mt-80 cursor-pointer">
-                <Image src={logo} alt="Logo" id="logo" />
-            </Link> */}
       <p
         id="citation-home"
         className="text-shadow-lg/500"
       >
         Bienvenue sur notre plateforme de gestion de vols aériens.Organisez,
-        suivez et optimisez vos trajets aériens en quelques clics.Notre système
+        suivez et optimisez vos trajets aériens en quelques clics.
+      </p>
+      <p
+        id="citation-home-2"
+        className="text-shadow-lg/500"
+      >
+        Notre système
         assure une coordination fluide entre passagers, vols et
         compagnies.Gagnez du temps grâce à une interface intuitive et des outils
         puissants.Avec nous, chaque vol est sous contrôle du décollage à
